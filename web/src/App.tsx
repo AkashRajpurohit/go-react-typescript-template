7df13679a248437027fb4d7ba8470243ff9ca564
AkashRajpurohit <me@akashrajpurohit.com>
2024-10-11 17:18:50 +0530

    feat: :truck: swap eslint with biome

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,22 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState('');
+	const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    fetch('/api/say-my-name')
-      .then((res) => res.text())
-      .then((data) => setMessage(data))
-      .catch((err) => console.error(err));
-  }, []);
+	useEffect(() => {
+		fetch("/api/say-my-name")
+			.then((res) => res.text())
+			.then((data) => setMessage(data))
+			.catch((err) => console.error(err));
+	}, []);
 
-  return (
-    <div>
-      <h1>Go + React</h1>
-      <p>Backend says: {message}</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Go + React</h1>
+			<p>Backend says: {message}</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
